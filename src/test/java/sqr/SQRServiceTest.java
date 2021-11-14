c6c5209f6c8601404fdda21c9c9441fc61ce13b0
Дмитрий Ц <appattit@mail.ru>
2021-11-14 18:30:27 +0300

    after tichers comments

---
+++ src/test/java/sqr/SQRServiceTest.java
@@ -0,0 +1,11 @@
+//package ru.netology.sqr;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//class SQRServiceTest {
+
+   // @org.junit.jupiter.api.Test
+   // @Test
+   // void sholdSqr() {
+    //}
+//}
